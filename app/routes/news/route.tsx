4193c706a8d2ec6f1f0661afbc53befd2d6c440f
jc <jcheese@treasure.lol>
2023-11-08 16:43:20 +0900

    more fixes and placements

--- app/routes/news/route.tsx
+++ app/routes/news/route.tsx
@@ -3,18 +3,63 @@ import CarVid from "./car.mp4";
 import Smol from "./smol.png";
 import { ClientOnly } from "remix-utils/client-only";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { Suspense, useEffect, useRef, useState } from "react";
+import { CSSProperties, Suspense, useEffect, useRef, useState } from "react";
 import {
   CameraControls,
   Scroll,
   ScrollControls,
   useTexture,
-  useVideoTexture
+  useVideoTexture,
+  Line,
+  Html
 } from "@react-three/drei";
 import { damp } from "maath/easing";
 import useStore from "./store";
 import { Icon } from "~/components/Icons";
 import * as THREE from "three";
+
+const material = new THREE.LineBasicMaterial({ color: "white" });
+const geometry = new THREE.BufferGeometry().setFromPoints([
+  new THREE.Vector3(0, -0.5, 0),
+  new THREE.Vector3(0, 0.5, 0)
+]);
+
+function Minimap() {
+  const ref = useRef<THREE.Group | null>(null);
+  const models = useStore((state) => state.models);
+  const { height } = useThree((state) => state.viewport);
+
+  const cameraIndex = useStore((state) => state.index);
+  useFrame((state, delta) => {
+    ref.current!.children.forEach((child, index) => {
+      const y =
+        cameraIndex === index
+          ? 1
+          : cameraIndex + 1 === index
+          ? 0.5
+          : cameraIndex - 1 === index
+          ? 0.5
+          : cameraIndex + 2 === index
+          ? 0.25
+          : cameraIndex - 2 === index
+          ? 0.25
+          : 0;
+      damp(child.scale, "y", 0.1 + y / 6, 0.1, delta);
+    });
+  });
+  return (
+    <group ref={ref}>
+      {models.map((_, i) => (
+        <line
+          key={i}
+          geometry={geometry}
+          material={material}
+          position={[i * 0.06 - models.length * 0.03, -height / 2 + 0.6, 0]}
+        />
+      ))}
+    </group>
+  );
+}
 
 const Newspaper = (
   props: JSX.IntrinsicElements["mesh"] & {
@@ -25,7 +70,8 @@ const Newspaper = (
   const controls = useThree(
     (state) => state.controls as unknown as CameraControls
   );
-
+  const setSelected = useStore((state) => state.setSelectedModel);
+  const selected = useStore((state) => state.selectedModel) === props.index;
   useEffect(() => {
     if (controls && controls.mouseButtons) {
       controls.mouseButtons.right = 0;
@@ -37,12 +83,10 @@ const Newspaper = (
     }
   }, []);
 
-  const [clicked, setClicked] = useState(false);
-
   const texture = useVideoTexture("videos/Issue_1.mp4");
 
   useEffect(() => {
-    if (clicked && ref.current) {
+    if (selected && ref.current) {
       controls?.fitToBox(ref.current, true, {
         paddingLeft: 0.05,
         paddingRight: 0.05,
@@ -56,14 +100,14 @@ const Newspaper = (
       controls?.reset(true);
       texture.image.muted = true;
     }
-  }, [clicked]);
+  }, [selected]);
 
   return (
     <mesh
       {...props}
       ref={ref}
-      onClick={() => setClicked((clicked) => !clicked)}
-      onPointerMissed={() => setClicked(false)}
+      onClick={() => setSelected(selected ? null : props.index)}
+      onPointerMissed={() => setSelected(null)}
     >
       <planeGeometry args={[1, 1, 1]} />
       <meshStandardMaterial map={texture} />
@@ -113,21 +157,32 @@ const Scroller = ({ w = 2, gap = 0.15 }) => {
 const Interface = () => {
   const next = useStore((state) => state.next);
   const previous = useStore((state) => state.previous);
+  const selectedModel = useStore((state) => state.selectedModel);
+
+  const notSelected = selectedModel === null;
   return (
-    <>
-      <button
-        className="absolute right-0 top-1/2 z-10 -translate-y-1/2 bg-neonPink/20 p-2 backdrop-blur-xl"
-        onClick={() => next()}
-      >
-        <Icon name="chevron-up" className="h-24 w-24 rotate-90" />
-      </button>
-      <button
-        className="absolute left-0 top-1/2 z-10 -translate-y-1/2 bg-neonPink/20 p-2 backdrop-blur-xl"
-        onClick={() => previous()}
-      >
-        <Icon name="chevron-up" className="h-24 w-24 -rotate-90" />
-      </button>
-    </>
+    <div
+      style={{
+        transition: "all 0.5s",
+        opacity: notSelected ? 1 : 0,
+        transform: `scale(${notSelected ? 1 : 0.25})`
+      }}
+    >
+      <div className="fixed bottom-4 left-1/2 -translate-x-1/2 space-x-4 sm:bottom-8">
+        <button
+          className="drounded-xl bg-black p-1.5 text-white backdrop-blur-xl transition-transform hover:scale-110"
+          onClick={() => previous()}
+        >
+          <Icon name="chevron-up" className="sm:h7 sm:w7 h-6 w-6 -rotate-90" />
+        </button>
+        <button
+          className="drounded-xl bg-black p-1.5 text-white backdrop-blur-xl transition-transform hover:scale-110"
+          onClick={() => next()}
+        >
+          <Icon name="chevron-up" className="sm:h7 sm:w7 h-6 w-6 rotate-90" />
+        </button>
+      </div>
+    </div>
   );
 };
 
@@ -156,6 +211,7 @@ export default function News() {
             />
             <Suspense fallback={null}>
               <Scroller />
+              <Minimap />
             </Suspense>
             <CameraControls makeDefault />
           </Canvas>
--- app/routes/news/store.ts
+++ app/routes/news/store.ts
@@ -2,15 +2,21 @@ import { create } from "zustand";
 
 interface State {
   index: number;
+  selectedModel: number | null;
+  setSelectedModel: (index: number | null) => void;
   models: undefined[];
   previous: () => void;
-  next: () => void;
+  next: (index?: number) => void;
 }
 
 export default create<State>((set) => {
   return {
     index: 0,
-    models: [...new Array(8).fill(undefined)],
+    models: [...new Array(12).fill(undefined)],
+    selectedModel: null,
+    setSelectedModel: (index) => {
+      set({ selectedModel: index });
+    },
     previous: () => {
       set((state) => {
         if (state.index > 0) return { index: state.index - 1 };
@@ -19,8 +25,9 @@ export default create<State>((set) => {
       });
     },
 
-    next: () => {
+    next: (index) => {
       set((state) => {
+        if (index) return { index };
         if (state.index < state.models.length - 1) {
           return { index: state.index + 1 };
         }
